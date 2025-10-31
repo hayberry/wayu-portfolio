e700c89c846969fcc2614f39f27a76fd45467ae4
hayberry <wayuberry@gmail.com>
2025-10-31 11:27:53 +0300

    fix in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/wayu-portfolio",
+  base: process.env.VITE_BASE_PATH || "/wayu-portfolio",
 });
